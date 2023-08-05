dbda7973b0606efcec590133d3bb684203052eca
DNasson <daniellememmott@hotmail.com>
2023-08-05 15:56:26 -0600

    "retry package.json, remove mentions of email"

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -9,7 +9,6 @@ router.post('/', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.email = userData.email;
       req.session.logged_in = true;
       res.status(200).json(userData);
     });
@@ -38,7 +37,7 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.email = userData.email;
+      req.session.username = userData.username;
       req.session.logged_in = true;
       res.json({ user: userData, message: 'You are now logged in!' });
     });
